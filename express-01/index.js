9869b260c47595e9a6b6a77ab9a24dc6ca7eb828
softdevomar <omorfarukqqq@gmail.com>
2023-11-11 01:10:17 -0800

    express js header perameter passing

--- express-01/index.js
+++ express-01/index.js
@@ -2,14 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+//query parameter passing 
+// app.get("/",(req,res)=>{
+//     const {id,name} =req.query; 
+//     res.send(`Student id is : ${id}  student name ${name}`);
+// })
+
+//route perameter passing
+// app.get("/userId/:id/userAge/:age",(req,res)=>{
+//     const {id,age} =req.params; 
+//     res.send(`Student id is : ${id}  student name ${age}`);
+// });
+
+// header perameter passing
 
 app.get("/",(req,res)=>{
-    // const id = req.query.id;
-    // const name = req.query.name;
-    const {id,name} =req.query; 
+    const id =req.header('id'); 
+    const name =req.header('name'); 
     res.send(`Student id is : ${id}  student name ${name}`);
 });
-
 
 app.listen(PORT,()=>{
   console.log(`Server is running at http://localhost:${PORT}`);
