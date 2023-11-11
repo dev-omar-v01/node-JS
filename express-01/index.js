406bde380b8f6e9cf90ad87c17dec74c2cec1086
softdevomar <omorfarukqqq@gmail.com>
2023-11-11 01:28:31 -0800

    send json body

--- express-01/index.js
+++ express-01/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const bodyParser = require('body-parser');
+
+
 
 //query parameter passing 
 // app.get("/",(req,res)=>{
@@ -16,10 +19,20 @@ const PORT = 3000;
 
 // header perameter passing
 
-app.get("/",(req,res)=>{
-    const id =req.header('id'); 
-    const name =req.header('name'); 
-    res.send(`Student id is : ${id}  student name ${name}`);
+// app.get("/",(req,res)=>{
+//     const id =req.header('id'); 
+//     const name =req.header('name'); 
+//     res.send(`Student id is : ${id}  student name ${name}`);
+// });
+
+
+//json body send 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.post('/',(req,res)=>{
+   const name = req.body.name;
+   res.send(`welcome ${name}`);
 });
 
 app.listen(PORT,()=>{
