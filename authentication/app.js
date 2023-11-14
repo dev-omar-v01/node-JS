fbbb2102c82e515892316a789c72d65a6b25efc1
softdevomar <omorfarukqqq@gmail.com>
2023-11-14 06:31:20 -0800

    lower lavel authentication

---
+++ authentication/app.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const User =require("./models/user.model");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const dbURL = process.env.MONGO_URL;
+
+mongoose.connect(dbURL).then(()=>{
+    console.log("mongoDB connection");
+}).catch((error)=>{
+    console.log("connection error");
+});
+
+
+app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+
+    res.send("home Route ");
+});
+
+
+app.post('/register',async (req,res)=>{
+    const {email,password} = req.body;
+    try{
+      const newUser = new User(req.body);
+       await  newUser.save();
+      res.status(201).json(newUser);
+    }catch(error){
+        res.status(500).json({
+            error: error.message,
+        });
+    }
+
+});
+
+
+app.post('/login',async (req,res)=>{
+    const {email,password} = req.body;
+    try{
+      const newUser = await User.findOne({email:email});
+      if(newUser && newUser.password == password){
+        res.status(200).json({message:"valid User"});
+
+      }else{
+        res.status(401).json({error:"user not valid"});
+      }
+    }catch(error){
+        res.status(500).json({
+            error: error.message,
+        });
+    }
+});
+
+
+
+
+
+app.use((err,req,res,next)=>{
+   res.status(500).json({
+    message: "something broke"
+   })
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is Running at http://localhost:${PORT}`);
+});
